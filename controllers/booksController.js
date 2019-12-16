1beb2a18f53a3702e4274f2b9c07ed35b7fcbc9d
Xavier Davis <xdavis1@icloud.com>
2019-12-16 05:40:07 -0800

    creating a find all method in controller folder

---
+++ controllers/booksController.js
@@ -0,0 +1,15 @@
+// importing the schema file from the models folder
+const db = require("../modeks");
+
+// Defining methods for the booksController
+module.exports ={
+    // defing the method for finding all books
+    findAll: function(req, res){
+        db.Book
+            .find(req.query)
+            .sort({ date: -1 })
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+    }
+
+}
