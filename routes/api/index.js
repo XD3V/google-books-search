82895c9702a1da69344eff9648adba8f19900018
Xavier Davis <xdavis1@icloud.com>
2019-12-16 06:04:53 -0800

    creating another book route file for exporting all the methods to the nessary files

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+// Setting up dependencies and imports
+const router = require("express").Router();
+const bookRoutes = require("./books");
+
+// Book routes
+router.use("/books", bookRoutes);
+
+module.exports = router;
