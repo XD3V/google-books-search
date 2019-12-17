7181a8dd997588ed1821a8e9091169d35e2674e7
Xavier Davis <xdavis1@icloud.com>
2019-12-17 11:13:23 -0800

    creating a grid component, so bootsrtap elements are able to be used so webpage can become more fluid

---
+++ client/src/components/Jumbotron/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+// This JumboTron component allows us to use a bootstrap Jumbotron without worrying about class names
+function Jumbotron({ children }) {
+  return (
+    <div
+      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+      className="jumbotron"
+    >
+      {children}
+    </div>
+  );
+}
+
+export default Jumbotron;
